4e7e2439076b4ea65adb69bf5b166a3d5576ade9
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-02-24 02:23:56 +0330

    commit message

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,6 +14,18 @@ document.addEventListener('mousemove', (event) => {
     openDropdown.classList.remove('active')
   }
 })
+// filter
+
+let openFilter = document.getElementById('openFilter');
+let sidebar = document.getElementById('sidebar');
+let closeFilter = document.getElementById('closeFilter');
+
+openFilter.addEventListener('click', function () {
+  sidebar.classList.add('active');
+});
+closeFilter.addEventListener('click', function () {
+  sidebar.classList.remove('active');
+});
 
 // select color
 
